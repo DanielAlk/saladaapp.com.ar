6ade1c557702e17e76cd089edb850046762e6488
DanielAlk <daniodfad@yahoo.com.ar>
2018-04-22 09:19:02 -0300

    changes since saladapanel is now in angular

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :retrieve_panel_data
+  #before_action :retrieve_panel_data
   #before_action :authenticate_panel, if: -> { request.subdomain.starts_with?('panel') }
 
   private
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,9 +2,9 @@ class Contact < ActiveResource::Base
   self.site = ENV['api_url']
   add_response_method :http_response
 
-  def self.unread
-    Contact.all(params: { f: { scopes: { read: 0 } }.to_json })
-  end
+  # def self.unread
+  #   Contact.all(params: { f: { scopes: { read: 0 } }.to_json })
+  # end
 
   schema do
     string :name
